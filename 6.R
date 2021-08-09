b98f31c87cdf88f6b8e3f3c08db290d4f7908e25
Jongseo-Lee <65481221+Jongseo-Lee@users.noreply.github.com>
2021-08-09 14:33:31 +0900

    practice

--- 6.R
+++ 6.R
@@ -1,15 +1,14 @@
 install.packages("dplyr")
-library(dplyr) # dplyr은 데이터 전처리 전문 패키지
+library(dplyr) # 데이터 분류
 exam <- read.csv("csv_exam.csv")
 exam
-
-exam %>% filter(class == 1) # 데이터 추출 방법
+exam %>% filter(class == 1) # 구분하기
 exam %>% filter(class != 1)
 exam %>% filter(math > 50)
 exam %>% filter(class == 1 & math >=50)
 exam %>% filter(math >= 90 | english >= 90)
 exam %>% filter(class == 1 | class == 3 | class == 5)
-exam %>% filter(class %in% c(1,3,5)) # 간편한 목록추출
+exam %>% filter(class %in% c(1,3,5)) # 간결하게 쓰기
 
 class1 <- exam %>% filter(class == 1)
 class2 <- exam %>% filter(class == 2)
@@ -23,9 +22,10 @@ exam %>% select(-math)
 
 exam %>% 
   filter(class == 1) %>%
-  select(english) # %>% 은 여러 개 조합 가능
+  select(english) # 섞어서 사용 가능
 
 exam %>% 
   select(id, math) %>% 
   head
+
 
